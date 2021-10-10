8ee115d5b8133a3424d3ced2ac9367481cb9e2a2
Pelumi527 <fatolupelumi@gmail.com>
2021-10-10 20:51:10 +0100

    update

--- src/components/ViewWinner/index.js
+++ src/components/ViewWinner/index.js
@@ -8,24 +8,30 @@ import { Tag3 } from '../../view/Raffle/Raffle.style';
 
 
 const ViewWinner = () => {
-    let url = 'https://rpc-mumbai.maticvigil.com'
+    const [count, setCount] = useState(1)
+    let url = 'https://rpc-mainnet.maticvigil.com/'
     let customHttpProvider = new ethers.providers.JsonRpcProvider(url);
-    const [round, setRound] = useState(1)
+    const [round, setRound] = useState(0)
     const [message, setMessage] = useState("");
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true)
     const [show, setShow] = useState(false);
 
-
+  console.log(LuckLadabi, 'abi code')
     const handleChange = (e) => {
         setRound(e.target.value)
       } 
 
+    const add = () => {
+        let day = 1
+        setCount(day)
+    }
+
+
     const fetchWinner = async () => {
-        const Luck = new ethers.Contract(contractAddress,LuckLadabi.abi, customHttpProvider);
-        console.log(round)
+        const Luck = new ethers.Contract(contractAddress,LuckLadabi, customHttpProvider);
         let result  = await Luck.viewWinners(round);
-        let winners = result.join(", ");
+        let winners = result.join(" , ");
         setMessage(winners)
     }
 
@@ -33,18 +39,18 @@ const ViewWinner = () => {
         <div>
             <label for="Days"><Tag3>Check winner for:</Tag3></label>
             <select className="select" onChange={handleChange} id="Days" name="Days">
-                <option value="1">Day 1</option>
-                <option value="2">Day 2</option>
-                <option value="3">Day 3</option>
-                <option value="4">Day 4</option>
-                <option value="5">Day 5</option>
+                <option value="0">Day 1</option>
+                <option value="1">Day 2</option>
+                <option value="2">Day 3</option>
+                <option value="3">Day 4</option>
+                <option value="4">Day 5</option>
             </select>
-            <Button  onClick={() => [fetchWinner(),handleShow()]}>Check winner</Button>
+            <Button  onClick={() => [fetchWinner(),handleShow(), add()]}>Check winner</Button>
             <Modal show={show} onHide={handleClose} animation={false}>
                     <Modal.Header closeButton>
                         <Modal.Title>Winner</Modal.Title>
                     </Modal.Header>
-                        <Modal.Body> <Raftag>Winners for day {round}: {message}</Raftag></Modal.Body>
+                        <Modal.Body> <Raftag>Winners for day {count}: {message}</Raftag></Modal.Body>
                     <Modal.Footer>
                     <Button variant="secondary" onClick={handleClose}>
                             Close
