fb955192439978793c70341c59cadcb6ee368694
AjayiMike <ajayimikeofficial@gmail.com>
2022-03-11 11:20:11 +0100

    add participant heading componenet, partipant and paticipants container component

--- src/view/Participants/index.js
+++ src/view/Participants/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
 import PageHeader from './components/pageHeader'
+import ParticipantsContainer from './components/participantsContainer'
+import styles from './index.module.css'
 
 const Participants = () => {
   return (
@@ -8,6 +10,11 @@ const Participants = () => {
       <Row>
         <Col sm = {12}>
           <PageHeader />
+        </Col>
+      </Row>
+      <Row>
+        <Col sm = {12}>
+          <ParticipantsContainer />
         </Col>
       </Row>
     </Container>
